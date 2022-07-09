05ea7e7741f652d215e80516f7f6a5d3ad6976d6
Ayush Sharma <EXCELAYUSH@GMAIL.COM>
2022-07-09 22:50:23 +0530

    commit

---
+++ Hardhat-dApp/constants/index.js
@@ -0,0 +1,6 @@
+//Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE06d87778c8e8A9d1806C40cf0350AF852B40896";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-d-app-eager-ayush.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
